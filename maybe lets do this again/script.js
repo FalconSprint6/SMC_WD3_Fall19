fc80380e800051a2b05cfaf8797be5dc31948a04
Juan Zitara <inxs96@hotmail.com>
2019-12-09 11:45:46 -0800

    Did stuff

--- maybe lets do this again/script.js
+++ maybe lets do this again/script.js
@@ -8,34 +8,46 @@ var addItem = $('.addCart');
      cartItems.innerHTML = count;
    }); // end add item function
    $("#add2Cart").click(function() {
-    $("#cart").addClass("animate").delay(150).queue(function(next) {
+    $(".fa-shopping-cart").addClass("animate").delay(150).queue(function(next) {
       $(this).removeClass("animate");
       next();
     });
   }); // end cart rotation function
-  
+  $(":input").blur(function(){
+      $("#form").children().each(function(){
+        if ($(this).val!==""){
+            $("#payment").prop("disabled", false)
+        }
+        else $("#payment").prop("disabled", true)
+      })
+  }); // end payment disable
+  $('#carouselExampleCaptions').carousel({
+    interval: 2500
+  }); // end carousel control
 });
-       function addressFunction() 
-  {
-      if (document.getElementById('fillbilling').checked)
-      {  
-          document.getElementById('billingfirstName').value = document.getElementById('shippingfirstName').value; 
-          document.getElementById('billinglastName').value = document.getElementById('shippinglastName').value;
-          document.getElementById('billingAddress').value = document.getElementById('shippingAddress').value;
-          document.getElementById('billingAddress2').value = document.getElementById('shippingAddress2').value;
-          document.getElementById('billingCountry').value = document.getElementById('shippingCountry').value;
-          document.getElementById('billingState').value = document.getElementById('shippingState').value;
-          document.getElementById('billingCity').value = document.getElementById('shippingCity').value;
-          document.getElementById('billingZip').value = document.getElementById('shippingZip').value;
+
+
+function addressFunction() 
+{
+    if (document.getElementById('fillbilling').checked)
+    {  
+        document.getElementById('billingfirstName').value = document.getElementById('shippingfirstName').value; 
+        document.getElementById('billinglastName').value = document.getElementById('shippinglastName').value;
+        document.getElementById('billingAddress').value = document.getElementById('shippingAddress').value;
+        document.getElementById('billingAddress2').value = document.getElementById('shippingAddress2').value;
+        document.getElementById('billingCountry').value = document.getElementById('shippingCountry').value;
+        document.getElementById('billingState').value = document.getElementById('shippingState').value;
+        document.getElementById('billingCity').value = document.getElementById('shippingCity').value;
+        document.getElementById('billingZip').value = document.getElementById('shippingZip').value;
+
   
-    
-  } else {  
-          document.getElementById('billingfirstName').value = '';   
-          document.getElementById('billinglastName').value = '';
-          document.getElementById('billingAddress').value = '';
-          document.getElementById('billingAddress2').value = '';
-          document.getElementById('billingCountry').value = '';
-          document.getElementById('billingState').value = '';
-          document.getElementById('billingCity').value = '';
-          document.getElementById('billingZip').value = '';       
-  }}; //end fill billing
+} else {  
+        document.getElementById('billingfirstName').value = '';   
+        document.getElementById('billinglastName').value = '';
+        document.getElementById('billingAddress').value = '';
+        document.getElementById('billingAddress2').value = '';
+        document.getElementById('billingCountry').value = '';
+        document.getElementById('billingState').value = '';
+        document.getElementById('billingCity').value = '';
+        document.getElementById('billingZip').value = '';       
+}}; //end fill billing
